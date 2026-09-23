a86d3d3187283f32a803e66334d9b7e8cf19bc2c
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-09-22 19:54:24 -0500

    Initial Commit

---
+++ justchill/justchillApp.swift
@@ -0,0 +1,17 @@
+//
+//  justchillApp.swift
+//  justchill
+//
+//  Created by Edgardo Muñoz on 22/09/26.
+//
+
+import SwiftUI
+
+@main
+struct justchillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
