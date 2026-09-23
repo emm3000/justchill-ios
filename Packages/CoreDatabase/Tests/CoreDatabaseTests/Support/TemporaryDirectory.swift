bed63c0cde8920d21b03571110177744ec3c548a
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-09-22 21:56:48 -0500

    test(database): remove temporary databases and ignore package lockfiles

--- Packages/CoreDatabase/Tests/CoreDatabaseTests/Support/TemporaryDirectory.swift
+++ Packages/CoreDatabase/Tests/CoreDatabaseTests/Support/TemporaryDirectory.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-struct TemporaryDirectory {
+final class TemporaryDirectory: Sendable {
     let url: URL
 
     init() throws {
         url = FileManager.default.temporaryDirectory.appending(path: UUID().uuidString, directoryHint: .isDirectory)
         try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+    }
+
+    deinit {
+        try? FileManager.default.removeItem(at: url)
     }
 
     var databaseURL: URL {
